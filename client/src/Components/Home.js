a506e90caccfe544447bb4f9cca5aae1423f4d5a
DarianC26 <dariancheung@gmail.com>
2023-03-25 15:11:04 -0400

    adding clouds, trying to animate

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import './Home.css'
 import {Link} from 'react-router-dom';
+import image1 from './images/cloud.png'
+
+function parallaxScroll() {
+  let cloud = document.getElementById('clouds');
+  window.addEventListener('scroll', () => {
+    let value = window.scrollY
+
+    cloud.style.right = value * -1.5 + 'px';
+  })
+}
 
 export default function Home() {
   return (
     <div className='container'>
       <div className='home_wrapper'>
         <nav className='nav_bar'>
-          <ul className='nav-links'>
+          <ul className='nav_links'>
                 <Link to="/login">
                     <li className='linker'>Login</li>
                 </Link>
@@ -17,6 +27,12 @@ export default function Home() {
             </ul>
         </nav>
         <div className='image_wrapper'></div>
+      </div>
+      <div className='transitioning'>
+        <img id='clouds' src={image1}></img>
+      </div>
+      <div className='second_page'>
+        <h1>hi</h1>
       </div>
     </div>
   );
