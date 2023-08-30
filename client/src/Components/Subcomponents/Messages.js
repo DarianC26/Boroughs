2cce93f5abfd3512e666a65f8d98e89e23472f05
DarianC26 <dariancheung@gmail.com>
2023-08-30 15:35:18 -0400

    messages successfully sent between two browsers

--- client/src/Components/Subcomponents/Messages.js
+++ client/src/Components/Subcomponents/Messages.js
@@ -3,12 +3,17 @@ import {useSocket} from '../Contexts/SocketProvider'
 
 export default function Messages() {
     const [users, getUsers] = useState("");
+    const [message, getMessage] = useState("");
     let socket = useSocket();
 
     useEffect(() => {
         if(socket != null) {
             socket.on("send-users", (data) => {
-                getUsers(data);
+                getUsers(data)
+            });
+
+            socket.on("receive-message", (data) => {
+                getMessage(data)
             });
         }
       }, [socket]);
@@ -23,20 +28,22 @@ export default function Messages() {
         console.log(users);
     }
 
+    function test2() {
+        console.log(message);
+    }
+
     function sendMessage() {
         let test = {
-            user: 'darianc',
+            user: 'DARIAN',
             msg: "This is a test message"
         }
-        socket.on("receive-message", (data) => {
-            console.log(data);
-        })
         socket.emit("send-message", test)
     }
 
     return (
         <div>
             <button onClick={test}>Test</button>
+            <button onClick={test2}>Test2</button>
             <button onClick={sendMessage}>Message</button>
         </div>
     )
